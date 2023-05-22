5b837be17464c7da6d55506f60ab36fc05da7505
Verona <valetta7777@list.ru>
2023-05-22 22:58:39 +0300

    chenges Gitignor

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int ticketPrice) {
+        int milesPerRub = 20;
+        int miles = ticketPrice / milesPerRub;
+
+        return miles;
+    }
+}
